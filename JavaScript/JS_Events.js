6bc7706dc02dbb7ada7148c7a9cf74b5bf142863
Vikash Yadav <vikashydv19@gmail.com>
2025-03-08 18:06:19 +0530

    JS Events

---
+++ JavaScript/JS_Events.js
@@ -0,0 +1,34 @@
+//Events in JS- code ke interesting changes ko khte hai
+
+// The change in the state of an object is known as an Event
+// Events are fired to notify code of "interesting changes" that may affect code execution.
+// Mouse events (click, double click etc.)
+// Keyboard events (keypress, keyup, keydown)
+// Form events (submit etc.)
+// Print event & many more
+
+//------------------------------------------------------------
+//  Event Handling in JS
+//  node.event = ( ) => {
+//  //handle here
+//  }
+//  example
+//  btn.onclick = ( ) => {
+//  console.log(“btn was clicked”);
+//  }
+
+
+//------------------------------------------------------------
+// Event Object
+// It is a special object that has details about the event.
+// All event handlers have access to the Event Object's properties and methods.
+// node.event = (e) => {
+//  //handle here
+//  }
+// e.target, e.type, e.clientX, e.clientY
+
+//----------------------------------------------------------
+// Event Listeners
+// node.addEventListener( event, callback ) 
+// node.removeEventListener( event, callback ) 
+// *Note : the callback reference should be same to remove
