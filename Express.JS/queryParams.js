2a7964c6790dac4db844363f8789eb22da6b23e6
Vikash Yadav <vikashydv19@gmail.com>
2025-03-16 12:16:41 +0530

    Route & Query Params

---
+++ Express.JS/queryParams.js
@@ -0,0 +1,43 @@
+//pagination, filtration, sort, search
+
+// /search?query=express&limit=10
+
+import express from "express";
+import path from "path";
+
+//creating instance
+const app = express();
+
+
+const staticPath= path.join(import.meta.dirname, "public"); 
+app.use("/public",express.static(staticPath))
+
+app.get("/profile/:username", (req,res) => { //[Object: null prototype] { username: 'vikash' }
+    console.log(req.params);
+    // console.log(req.params.username);
+    res.send(`<h1>You typed ${req.params.username}</h1>`);
+}); //dynamic route se access kiya
+
+app.get("/profile/:username/article/:slug", (req,res) => { 
+    console.log(req.params);
+    const formatedSlug= req.params.slug.replace(/-/g," "); //global replacement
+    res.send(`<h1>Article ${formatedSlug} written by ${req.params.username}</h1>`);
+}); 
+
+app.get("/product", (req,res) => { //we will still get empty object- req.query in case of single path type
+    console.log(req.query);
+    
+    res.send(`<h1>Product Page</h1>`);
+});
+/*
+For-product?search=kuch-nahi&search2=abhi-bhi-kuch-nhi
+Output- [Object: null prototype] {
+  search: 'kuch-nahi',
+  search2: 'abhi-bhi-kuch-nhi'
+}
+  */
+
+const PORT= 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running at port: ${PORT}`);
+})
---
+++ Express.JS/routeParams.js
@@ -0,0 +1,28 @@
+import express from "express";
+import path from "path";
+
+//creating instance
+const app = express();
+
+
+const staticPath= path.join(import.meta.dirname, "public"); 
+app.use("/public",express.static(staticPath))
+
+app.get("/profile/:username", (req,res) => { //[Object: null prototype] { username: 'vikash' }
+    console.log(req.params);
+    // console.log(req.params.username);
+    res.send(`<h1>You typed ${req.params.username}</h1>`);
+}); //dynamic route se access kiya
+
+app.get("/profile/:username/article/:slug", (req,res) => { 
+    console.log(req.params);
+    const formatedSlug= req.params.slug.replace(/-/g," "); //global replacement
+    res.send(`<h1>Article ${formatedSlug} written by ${req.params.username}</h1>`);
+}); 
+
+// express hme automatically ek object return krta h jaha pe dynamic url is called property or key
+
+const PORT= 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server is running at port: ${PORT}`);
+})
